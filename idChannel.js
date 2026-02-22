ffbce1bd73a2bd436f912e93dc8354e6de79ef97
ArulXzOfficial-ID <dimassaputra1566@gmail.com>
2026-02-22 14:03:57 +0700

    Add channel IDs and descriptions to idChannel.js

--- idChannel.js
+++ idChannel.js
@@ -1,1 +1,8 @@
-
+[
+  { "id": "120363407086048404@newsletter", "desc": "About Elminator" },
+  { "id": "120363425287276536@newsletter", "desc": "Arul Channel #1" },
+  { "id": "120363425608572347@newsletter", "desc": "Arul Channel #2" },
+  { "id": "120363423044288688@newsletter", "desc": "Arul Channel #3" },
+  { "id": "120363425994469760@newsletter", "desc": "Arul Channel #4" },
+  { "id": "120363404258144013@newsletter", "desc": "Arul Channel #5" }
+]
